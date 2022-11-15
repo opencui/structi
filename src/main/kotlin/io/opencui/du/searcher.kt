e6f96022d1e70b28a4a510c62fa5a7584e9311d7
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2022-11-15 01:42:33 +0000

    add more log

--- src/main/kotlin/io/opencui/du/searcher.kt
+++ src/main/kotlin/io/opencui/du/searcher.kt
@@ -316,7 +316,7 @@ data class ExpressionSearcher(val agent: DUMeta) {
                 val slotName = it.value.removePrefix("<").removeSuffix(">").removeSurrounding(" ")
                 val triggers = expr.bot.getSlotMeta(expr.owner, slotName)?.triggers
                 if (triggers.isNullOrEmpty()) {
-                    throw RuntimeException("Missing trigger for $slotName in ${expr.owner}.")
+                    throw RuntimeException("Missing trigger for $slotName in ${expr.owner} for ${expr.utterance}.")
                 } else {
                     "< ${triggers[0]} >"
                 }
