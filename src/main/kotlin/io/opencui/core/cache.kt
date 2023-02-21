e38812478690c215307874cd9488bdf8658b7ea4
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2023-02-21 02:06:45 +0000

    add cache

--- src/main/kotlin/io/opencui/core/cache.kt
+++ src/main/kotlin/io/opencui/core/cache.kt
@@ -1,5 +1,7 @@
 package io.opencui.core
 
+import java.time.Duration
+import java.time.LocalDateTime
 import java.util.concurrent.TimeUnit
 
 interface Cache<Key, Value> {
@@ -126,4 +128,39 @@ class ExpirableCache<Key, Value>(
 
 interface Recyclable {
     fun recycle()
+}
+
+/**
+ * Add support for the common function cache.
+ */
+class CachedMethod2<A, B, out R>(
+    val f: (A, B) -> List<R>, val values: MutableMap<Pair<A, B>, Pair<List<@UnsafeVariance R>, LocalDateTime>>
+) : (A, B) -> List<R> {
+    private val seconds = 60
+    override fun invoke(a: A, b: B): List<R> {
+        val input = Pair(a, b)
+        val cache = values[input]
+        Dispatcher.logger.debug("Enter cached function... for $a and $b")
+        if (cache == null || Duration.between(cache!!.second, LocalDateTime.now()).seconds > seconds) {
+            Dispatcher.logger.debug("for some reason we need to refresh: $cache and ${LocalDateTime.now()}")
+            values.put(input, Pair(f(a, b), LocalDateTime.now()))
+        }
+        return values[input]!!.first
+    }
+}
+
+class CachedMethod3<A, B, C, out R>(
+    val f: (A, B, C) -> List<R>, val values: MutableMap<Triple<A, B, C>, Pair<List<@UnsafeVariance R>, LocalDateTime>>
+) : (A, B, C) -> List<R> {
+    private val seconds = 60
+    override fun invoke(a: A, b: B, c: C): List<R> {
+        val input = Triple(a, b, c)
+        val cache = values[input]
+        Dispatcher.logger.debug("Enter cached function... for $a and $b")
+        if (cache == null || Duration.between(cache!!.second, LocalDateTime.now()).seconds > seconds) {
+            Dispatcher.logger.debug("for some reason we need to refresh: $cache and ${LocalDateTime.now()}")
+            values.put(input, Pair(f(a, b, c), LocalDateTime.now()))
+        }
+        return values[input]!!.first
+    }
 }
