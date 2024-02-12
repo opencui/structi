beac134debdba2e113f9f53f3290be5209c5f952
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2024-02-12 06:47:07 +0000

    Not all user info need extra function all

--- src/main/kotlin/io/opencui/sessionmanager/SessionManager.kt
+++ src/main/kotlin/io/opencui/sessionmanager/SessionManager.kt
@@ -78,9 +78,11 @@ class SessionManager(private val sessionStore: ISessionStore, val botStore: IBot
         if (channel.isVerfied) {
             // We fetch these info when we first create user session.
             val identifier = bot.getChannel(channel.channelLabel!!)!!.getIdentifier(botInfo, channel.userId!!)
-            createdSession.name = identifier.name
-            createdSession.phone = identifier.phone
-            createdSession.email = identifier.email
+            if (identifier != null) {
+                createdSession.name = identifier.name
+                createdSession.phone = identifier.phone
+                createdSession.email = identifier.email
+            }
         }
 
         sessionStore.saveSession(channel.channelId(), channel.userId!!, botInfo, createdSession)
