c6ab9f401f13d13c1a5ff816bfc99bffcf8adf11
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2024-06-07 23:53:00 +0100

    using closure with reciever

--- src/main/kotlin/io/opencui/core/compiled.kt
+++ src/main/kotlin/io/opencui/core/compiled.kt
@@ -888,6 +888,9 @@ data class BadIndex(override var session: UserSession? = null, var index: Int) :
     }
 }
 
+// This turns a closure with receiver
+fun <T, P> bindReceiver1(lambda: T?.(P) -> Boolean, t: T?): (P) -> Boolean = { p -> t == null || t.lambda(p) }
+
 // This for and, used at out most as filters.
 fun <T> List<(T) -> Boolean>.invoke(t: T) : Boolean {
     for (filter in this) {
@@ -913,6 +916,24 @@ data class Or<T>(val filters: List<(T)->Boolean>) : (T) -> Boolean {
         return false
     }
 }
+
+data class Filter<T, P>(
+    val test: T?.(P) -> Boolean,
+    val originalValue: T?,
+    val negateValues: List<T> = emptyList(),
+    val orValues: List<T> = emptyList()) : (P) -> Boolean {
+
+    private val testers = listOf(
+        bindReceiver1(test, originalValue),
+        Negate(negateValues.map { bindReceiver1(test, it)}),
+        Or(orValues.map { bindReceiver1(test, it)})
+    )
+
+    override fun invoke(p1: P): Boolean {
+        return testers.invoke(p1)
+    }
+}
+
 
 data class PagedSelectable<T: Any> (
     override var session: UserSession? = null,
