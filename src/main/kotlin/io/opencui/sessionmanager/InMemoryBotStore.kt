1bd5d3d93a83a8dbfbb3fac5d9ebec747b54d5a5
xiaoyun wu <xiaoyun.wu@gmail.com>
2024-08-09 17:07:28 -0700

    small cleanup (#301)

--- src/main/kotlin/io/opencui/sessionmanager/InMemoryBotStore.kt
+++ src/main/kotlin/io/opencui/sessionmanager/InMemoryBotStore.kt
@@ -2,31 +2,34 @@ package io.opencui.sessionmanager
 
 import io.opencui.core.BotInfo
 
+
+// This is for testing, so we should try to separate from branching.
 data class InMemoryBotStore(override val botInfo: BotInfo): IBotStore {
     private val cache = HashMap<String, String>()
     private val listCache = HashMap<String, MutableList<String>>()
+
     override fun set(key: String, version: String): Boolean {
-        cache["agent:${botInfo.fullName}:${botInfo.lang}:${botInfo.branch}|$key"] = version
+        cache[getKey(key)] = version
         return true
     }
 
     override fun get(key: String): String? {
-        return cache["agent:${botInfo.fullName}:${botInfo.lang}:${botInfo.branch}|$key"]
+        return cache[getKey(key)]
     }
 
     override fun rpush(key: String, value: String) {
-        if (!listCache.containsKey(key)) {
-            listCache.put(key, mutableListOf())
+        if (!listCache.containsKey(getKey(key))) {
+            listCache.put(getKey(key), mutableListOf())
         }
-        listCache[key]!!.add(value)
+        listCache[getKey(key)]!!.add(value)
     }
 
     override fun lrange(key: String, start: Int, end: Int): List<String> {
-        return if (!listCache.containsKey(key)) emptyList() else listCache[key]!!
+        return if (!listCache.containsKey(getKey(key))) emptyList() else listCache[getKey(key)]!!
     }
 
     override fun lrem(key: String, value: String) : Int {
-        return if (listCache[key]?.remove(value) == true) 1 else 0
+        return if (listCache[getKey(key)]?.remove(value) == true) 1 else 0
     }
 
     override fun toString(): String {
