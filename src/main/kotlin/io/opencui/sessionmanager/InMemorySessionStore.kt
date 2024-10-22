8a6758f105d80dd2a32b072f270cfeb736a4c2a1
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2024-10-23 00:07:19 +0100

    to catch serialization error in debug mode

--- src/main/kotlin/io/opencui/sessionmanager/InMemorySessionStore.kt
+++ src/main/kotlin/io/opencui/sessionmanager/InMemorySessionStore.kt
@@ -4,12 +4,20 @@ import io.opencui.core.BotInfo
 import io.opencui.core.ExpirableCache
 import io.opencui.core.PerpetualCache
 import io.opencui.core.UserSession
+import io.opencui.sessionmanager.ISessionStore.Companion.decodeSession
+import io.opencui.sessionmanager.ISessionStore.Companion.encodeSession
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class InMemorySessionStore: ISessionStore {
-    val cache = ExpirableCache<String, UserSession>(PerpetualCache(), TimeUnit.MINUTES.toNanos(30))
+    val cache = ExpirableCache<String, String>(PerpetualCache(), TimeUnit.MINUTES.toNanos(30))
+
     override fun getSession(channel: String, id:String, botInfo: BotInfo): UserSession? {
-        return cache[ISessionStore.key(channel, id, botInfo)]
+        val encodedSession= cache[ISessionStore.key(channel, id, botInfo)] ?: return null
+        val customClassLoader = ChatbotLoader.findClassLoader(botInfo)
+        return decodeSession(encodedSession, customClassLoader)
     }
 
     override fun deleteSession(channel: String, id:String, botInfo: BotInfo) {
@@ -19,11 +27,11 @@ class InMemorySessionStore: ISessionStore {
     override fun updateSession(channel: String, id: String, botInfo: BotInfo, session: UserSession) {
         val key = ISessionStore.key(channel, id, botInfo)
         if (cache[key] != null) {
-            cache[key] = session
+            cache[key] = encodeSession(session)
         }
     }
 
     override fun saveSession(channel: String, id: String, botInfo: BotInfo, session: UserSession) {
-        cache[ISessionStore.key(channel, id, botInfo)] = session
+        cache[ISessionStore.key(channel, id, botInfo)] = encodeSession(session)
     }
 }
