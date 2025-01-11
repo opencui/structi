2a904dbbeb22f536c94335cd23530e00b891396f
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2025-01-11 05:35:57 +0000

    add support for kafka logger

--- src/main/kotlin/io/opencui/logger/ILogger.kt
+++ src/main/kotlin/io/opencui/logger/ILogger.kt
@@ -3,12 +3,25 @@ package io.opencui.logger
 import io.opencui.core.Configuration
 import io.opencui.core.ExtensionBuilder
 import io.opencui.core.IExtension
+import io.opencui.serialization.Json
 import io.opencui.serialization.JsonElement
+import io.opencui.serialization.JsonObject
+import org.apache.kafka.clients.producer.Callback
 import java.sql.*
 import java.time.LocalDateTime
 import org.postgresql.util.PGobject
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import java.util.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+import org.apache.kafka.common.serialization.StringSerializer
+import java.io.Closeable
 
 data class Turn(
     val utterance: String,
@@ -20,19 +33,22 @@ data class Turn(
     // an array of dialog acts.
     var dialogActs: JsonElement?= null
     var trueFrameEvents: JsonElement? = null  // this is provided manually when there are mistakes
+    var timeStamp: LocalDateTime? = null
     var dmTime: Long? = null // We might need this.
     var nluVersion: String? = null
     var duVersion: String? = null
     var channelType: String? = null
     var channelLabel: String? = null
+    var sessionId: String? = null
+    // along with channelType and channel label, this uniquely identify a
+    var messageId: String? = null
     var userId: String? = null
+    var usage: JsonElement? = null
 }
 
 interface ILogger: IExtension {
     fun log(turn: Turn): Boolean
 }
-
-
 
 data class Inserter(val stmt: PreparedStatement) {
     var index: Int = 1
@@ -57,7 +73,6 @@ data class Inserter(val stmt: PreparedStatement) {
 }
 
 data class JdbcLogger(val info: Configuration): ILogger {
-
     init {
         Class.forName("org.postgresql.Driver")
     }
@@ -65,7 +80,6 @@ data class JdbcLogger(val info: Configuration): ILogger {
     val conn : Connection by lazy {
         DriverManager.getConnection(info[URL] as String)
     }
-
 
     override fun log(turn: Turn): Boolean {
         val builder = Inserter(conn.prepareStatement(sqlStatement));
@@ -101,6 +115,60 @@ data class JdbcLogger(val info: Configuration): ILogger {
 
         override fun invoke(p1: Configuration): ILogger {
             logger.info("Creating JdbcLogger for ${p1[URL]}")
+            return JdbcLogger(p1)
+        }
+
+    }
+}
+
+// Eventually, we should move the KafkaLogger as it support async operation.
+class KafkaLogger(val info: Configuration): ILogger, Closeable{
+    val conn : KafkaProducer<String, String> = KafkaProducer<String, String>(getProducerProperties(info))
+    val topic: String = info[KAFKA_TOPIC]!! as String
+    val key: String = info[KAFKA_KEY]!! as String
+
+    fun getProducerProperties(info: Configuration): Properties {
+        val broker = info[KAFKA_BROKER]!! as String
+        return Properties().apply {
+            put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, broker)
+            put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+            put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+            put(ProducerConfig.ACKS_CONFIG, "all")
+        }
+    }
+
+    override fun close() {
+        conn.close()
+    }
+
+    override fun log(turn: Turn): Boolean {
+        val jsonValue = Json.encodeToString(turn)
+
+        try {
+            val record = ProducerRecord(topic, key, jsonValue)
+            conn.send(record, Callback { metadata, exception ->
+                if (exception != null) {
+                    // Handle the exception
+                    println("Error sending record(key=$key): ${exception.message}")
+                } else {
+                    // Log success
+                    println("Sent record(key=$key value=$jsonValue) " +
+                        "to partition=${metadata.partition()} offset=${metadata.offset()}")
+                }
+            })
+        } catch (e: Exception) {
+            logger.info("Insert $turn got ${e.toString()}")
+        }
+        return true
+    }
+
+    companion object : ExtensionBuilder {
+        val logger: Logger = LoggerFactory.getLogger(KafkaLogger::class.java)
+        val KAFKA_BROKER = "KAFKA_BROKER"
+        val KAFKA_TOPIC = "KAFKA_TOPIC"
+        val KAFKA_KEY = "KAFKA_KEY"
+
+        override fun invoke(p1: Configuration): ILogger {
             return JdbcLogger(p1)
         }
 
