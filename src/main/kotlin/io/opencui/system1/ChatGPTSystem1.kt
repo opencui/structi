4c77ed11a19130207f057b91b89ff6e6d1509288
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2025-01-31 21:33:45 +0000

    update system1 to support <think>

--- src/main/kotlin/io/opencui/system1/ChatGPTSystem1.kt
+++ src/main/kotlin/io/opencui/system1/ChatGPTSystem1.kt
@@ -13,13 +13,26 @@ fun List<CoreMessage>.convert(): List<OpenAIMessage> {
     return this.map { OpenAIMessage(if (it.user) "user" else "assistant", it.message) }
 }
 
+fun extractAfterXMLTag(input: String, tag: String): String {
+    val count = input.windowed(tag.length, 1).count { it == tag }
+
+    return when (count) {
+        1 -> input.substring(input.indexOf(tag) + tag.length) // Return substring after </think>
+        0 -> input // No occurrence, return entire string
+        else -> "" // More than one occurrence, return empty string
+    }
+}
+
 
 // Feedback is only useful when turns is empty.
 data class System1Request(val turns: List<OpenAIMessage>, val feedback: Map<String, Any>? = null)
 
-data class System1Reply(val reply: String)
+data class System1Reply(val reply: String) 
 
-data class ChatGPTSystem1(val url: String) : ISystem1 {
+data class ChatGPTSystem1(val config: Configuration) : ISystem1 {
+    val url = config[urlKey]!! as String
+    val thinkFlag: Boolean = config[THINK] as Boolean? ?: false
+
     val client = WebClient.builder()
       .baseUrl(url)
       .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
@@ -31,14 +44,23 @@ data class ChatGPTSystem1(val url: String) : ISystem1 {
             .body(Mono.just(request), System1Request::class.java)
             .retrieve()
             .bodyToMono(System1Reply::class.java)
-        return response.block()!!.reply
+
+        val reply = response.block()!!.reply
+
+        // handle the think.
+        return if (reply.startsWith(THINKSTART))
+            extractAfterXMLTag(reply, THINKEND)
+        else
+            reply
     }
 
     companion object : ExtensionBuilder {
         override fun invoke(p1: Configuration): ISystem1 {
-            val url = p1[urlKey]!! as String
-            return ChatGPTSystem1(url)
+            return ChatGPTSystem1(p1)
         }
         const val urlKey = "url"
+        const val THINK = "THINK"
+        const val THINKSTART = "<think>"
+        const val THINKEND = "</think>"
     }
 }
